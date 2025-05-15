6e3e93429e2abc222622cc66a0a4457f07727d69
Alok Ratheesh <alokratheesh314@gmail.com>
2025-05-15 22:38:10 +0400

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,8 @@
+# This is one of Hello_World programs desgined to test the functionality of qiskit and understand the work flow better
 
+Learned it from Qiskit Youtube channel and was one of the very first programs I wrote 
+This is basically something known as a $Bell State$ in Qunatum Computing which leaves us with an Entangled state 
+
+Here we are implementing a 2 qubit Bell state running it locally on a simulator called as the *Aer Simulator* 
+
+In Qiskit we can also run the program on a real quantum hardware, but for this simple code using only 2 qubits a simulator would suffice
